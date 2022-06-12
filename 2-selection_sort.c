09ebfff6964eaf47c77cf54239a394a6f8895e68
abdou50 <abdelkaderraddadi988@gmail.com>
2022-06-12 15:01:06 +0300

    l

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,28 +1,28 @@
 #include "sort.h"
 /**
- *selection_sort - function
- *@array: tableau
- *@size: size
+ * selection_sort - function that range
+ * @array: array of integers
+ * @size: size of array
  */
 void selection_sort(int *array, size_t size)
 {
+	int temp;
 	size_t i, j, min;
-	int  temp;
 
 	for (i = 0; i < size; i++)
 	{
 		min = i;
-		for (j = j + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
-			if (min != i)
-			{
-				temp = array[min];
-				array[min] = array[i];
-				array[i] = temp;
-				print_array(array, size);
-			}
+		}
+		if (min != i)
+		{
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+			print_array(array, size);
 		}
 	}
 }
